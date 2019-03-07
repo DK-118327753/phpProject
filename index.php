be910b9e82121e59f786c3cbde3c90d4e5e86563
118327753 <118327753@BISBIS-U48UJOJN.bis.ad.ucc.ie>
2019-03-07 17:00:05 +0000

    Second Commit

--- index.php
+++ index.php
@@ -7,11 +7,18 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Home Page</title>
     </head>
     <body>
+        <h1>Home</h1>
         <?php
-     echo 'Hello World'; //How to print words in PHP
+     echo 'Hello, welcome to the homepage of my website.';
+     echo 'Here you will be able to access all the available features of my website, view my CV and personal interests';
+     echo 'and access my consulting service where you can purchase a consultation programme from a range of options.';
         ?>
+        
+        <a href="CV.html"><button type="Button" class="btn btn-primary btn-block">CV Information</button>
+        <a href="Interests.html"><button type="Button" class="btn btn-primary btn-block">Interests Information</button>
+        <a href="ConsultingService.html"><button type="Button" class="btn btn-primary btn-block">Consultation Services</button>
     </body>
 </html>
